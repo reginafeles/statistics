273c18c52d85e1cfc2103064f62ff847eee4648f
Eliseykina Kate <90447294+reginafeles@users.noreply.github.com>
2023-03-18 21:04:43 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,23 @@
+pron1sg = ['pron1sg', 'NPRO', '1per', 'sing']
+pron1pl = ['pron1pl', 'NPRO', '1per', 'plur']
+
+pron2sg = ['pron2sg', 'NPRO', '2per', 'sing']
+pron2pl = ['pron2pl', 'NPRO', '2per', 'plur']
+
+pronouns = [pron1sg,pron1pl, pron2sg, pron2pl]
+
+comparative = ['comparative', 'COMP']
+superlative = ['superlative', 'Supr']
+imperative = ['imperative', 'impr']
+
+tags = [comparative, superlative, imperative]
+
+comfortable = ['comfortable', 'легко', 'удобн']
+real = ['real', 'действительн', 'реальн']
+surely = ['surely', 'обязательн']
+free = ['free', 'бесплатн']
+profitably = ['profitably', 'выгод']
+absolutely = ['absolutely', 'абсолютн']
+very = ['very', 'очень']
+
+adverbs = [comfortable, real, surely, free, profitably, absolutely, very]
