78e31f1a5d039192689569c6c3e5320890c14357
Eliseykina Kate <90447294+reginafeles@users.noreply.github.com>
2023-03-18 21:05:36 +0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,17 +1,16 @@
 import pymorphy2
 import re
+from constants import tags, pronouns, adverbs
 
 
 class Pipeline:
 
     def __init__(self, text):
         self.text = text
-        self.person1 = ['person1', '1per']
-        self.person2 = ['person2', '2per']
-        self.comparative = ['comparative', 'COMP']
-        self.superlative = ['superlative', 'Supr']
-        self.tags_dict = {'person1': 0, 'person2': 0, 'comparative': 0, 'superlative': 0, 'numbers': 0}
-        self.aims = [self.person1, self.person2, self.comparative, self.superlative]
+        self.tags_dict = {'pron1sg': 0, 'pron1pl': 0, 'pron2sg': 0, 'pron2pl': 0, 'comparative': 0, 'superlative': 0, 'numbers': 0, 'imperative': 0}
+        self.adverbs_dict = {'comfortable': 0, 'real': 0, 'surely': 0, 'free': 0, 'profitably': 0, 'absolutely': 0, 'very': 0}
+        self.tags = tags
+        self.adverbs = adverbs
 
     def get_cleaned(self):
 
@@ -20,18 +19,44 @@ class Pipeline:
 
         return self.text
 
+    def get_numbers(self):
+
+        for word in self.text:
+            if re.search('[0-9]+', word):
+                self.tags_dict['numbers'] += 1
+    def get_pron(self):
+
+        morph = pymorphy2.MorphAnalyzer()
+        for word in self.text:
+            parsed = morph.parse(word)[0].tag
+            for pron in pronouns:
+                if pron[1] in parsed and pron[2] in parsed and pron[3] in parsed:
+                    self.tags_dict[pron[0]] += 1
+
     def get_tags(self):
 
         morph = pymorphy2.MorphAnalyzer()
         for word in self.text:
-            for aim in self.aims:
-                for i in aim:
+            for aim in self.tags:
+                for tag in aim:
                     try:
-                        if i in morph.parse(word)[0].tag:
+                        if tag in morph.parse(word)[0].tag:
                             self.tags_dict[aim[0]] += 1
                     except:
                         continue
-                    if re.search('[0-9]+', word):
-                        self.tags_dict['numbers'] += 1
-
+        self.get_pron()
+        self.get_numbers()
         return self.tags_dict
+
+    def get_adverbs(self):
+
+        for word in self.text:
+            for aim in self.adverbs:
+                for adverb in aim:
+                    if adverb in word:
+                        self.adverbs_dict[aim[0]] += 1
+        return self.adverbs_dict
+
+    def get_freq(self):
+        self.get_tags()
+        self.get_adverbs()
